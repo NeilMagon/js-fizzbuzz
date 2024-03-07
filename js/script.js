db41dad5fe2b792490cad65ede53b7adcea9e06d
NeilMagon <neil.magon97@gmail.com>
2024-03-07 17:21:13 +0100

    feat: fix js

--- js/script.js
+++ js/script.js
@@ -4,37 +4,47 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // selezione il container
-const container = document.querySelector(`#container`);
+const numContainer = document.querySelector(`#container`);
 console.log(container)
 // creo tutti i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    console.log(i);
+    const newBox = document.createElement(`div`)
+    // creo la variabile classText
+    let classText;
+    console.log(classText);
     // creo la variabile fizzBuzz
     let fizzBuzz = i;
     // se diviso per 3 e 5 da riporto 0 scrive FizzBuzz
     if (i % 3 === 0 && i % 5 === 0) {
         fizzBuzz = `fizzbuzz`;
+        classText = `fizzbuzz`;
         // se diviso per 3 da riporto 0 scrive Fizz
     } else if (i % 3 === 0) {
         fizzBuzz = `fizz`;
+        classText = `fizz`;
         // se diviso per 5 da riporto 0 scrive Buzz
     } else if (i % 5 === 0) {
         fizzBuzz = `buzz`;
+        classText = `buzz`;
+    } else {
+        classText = ``;
     }
-    console.log(fizzBuzz)
-    const numberBox = `<div class="box ${fizzBuzz}">${i}</div>`;
+    console.log(fizzBuzz);
+
+    const numberBox = `<div class="box ${classText}">${fizzBuzz}</div>`;
     console.log(numberBox)
 
-    container.innerHTML += numberBox;
+    numContainer.innerHTML += numberBox;
 
     // const numberBox = document.createElement('div');
 
-    // numberBox.innerHTML = i;
+    // numberBox.innerHTML = fizzBuzz;
     // numberBox.classList.add('box');
-    // numberBox.classList.add(fizzBuzz);
+    // numberBox.classList.add(classText);
     // console.log(numberBox);
 
-    // container.append(numberBox)
+    // numContainer.append(numberBox)
 }
 
 
